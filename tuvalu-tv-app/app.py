89b835d8c5bf9c85e06ce8eda4c0f07210dff0f8
moliageoffrey <59656094+moliageoffrey@users.noreply.github.com>
2026-02-25 21:02:03 +1100

    Remove specific Facebook video embed links
    Removed the Facebook embed link for a specific video and alternative page plugin.

--- tuvalu-tv-app/app.py
+++ tuvalu-tv-app/app.py
@@ -8,13 +8,6 @@ st.subheader("Featured Broadcast")
 
 # This is the most stable version of the Facebook Embed URL
 # It points directly to the video ID 1AfPd5mmTs
-fb_embed_link = "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2Fvideos%2F10153231339965444%2F&show_text=0&width=560" 
-
-# Alternative: If the specific video is being stubborn, 
-# it is better to embed the whole Page Feed so it always shows the LATEST video.
-page_plugin = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTuvaluBroadcastingCorporation&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
-
-# Try the video first:
 st.subheader("Latest from Tuvalu TV")
 # This shows the actual Facebook Page Feed
 st.components.v1.iframe("https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTuvaluBroadcastingCorporation&tabs=timeline&width=500&height=500", height=500)
