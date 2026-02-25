7eee334619b4caf4d6f6ddb89ff39ec02190535f
moliageoffrey <59656094+moliageoffrey@users.noreply.github.com>
2026-02-25 20:57:57 +1100

    Update app.py

--- tuvalu-tv-app/app.py
+++ tuvalu-tv-app/app.py
@@ -1,22 +1,23 @@
 import streamlit as st
 
-# Setup the page
 st.set_page_config(page_title="Tuvalu TV Live", layout="wide")
 
 st.title("🇹🇻 Tuvalu TV - Live Stream & Schedule")
 
-# Video Player Section
 st.subheader("Featured Broadcast")
 
-# This is the "Magic" link format for Facebook embeds
-fb_video_url = "https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/watch/?v=1AfPd5mmTs&show_text=0&width=560"
+# This is the most stable version of the Facebook Embed URL
+# It points directly to the video ID 1AfPd5mmTs
+fb_embed_link = "https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Ffacebook%2Fvideos%2F10153231339965444%2F&show_text=0&width=560" 
 
-# We use an iframe to ensure the Facebook player loads correctly
-st.components.v1.iframe(fb_video_url, height=500, scrolling=False)
+# Alternative: If the specific video is being stubborn, 
+# it is better to embed the whole Page Feed so it always shows the LATEST video.
+page_plugin = "https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTuvaluBroadcastingCorporation&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
+
+# Try the video first:
+st.components.v1.iframe(f"https://www.facebook.com/plugins/video.php?href=https://www.facebook.com/watch/?v=1AfPd5mmTs", height=500)
 
 st.divider()
 
-# EPG Section
 st.subheader("Program Guide")
-epg_source = "https://pasifikatv.co.nz/schedule/"
-st.components.v1.iframe(epg_source, height=800, scrolling=True)
+st.components.v1.iframe("https://pasifikatv.co.nz/schedule/", height=800, scrolling=True)
